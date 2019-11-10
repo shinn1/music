176aede9ae0f2f256c383a1e95695d95180b7ce1
shinnakayama <shinn407@gmail.com>
2019-11-10 12:21:47 -0800

    Update recurrence_together.py

--- recurrence_together.py
+++ recurrence_together.py
@@ -5,24 +5,25 @@ import time
 from pathlib import Path
 
 # import functions for recurrence analysis
-home = str(Path.home())
-os.chdir(home + '/Google Drive/music_project/codes_python')
 from recurrence_functions import symbolize, recurrence
 
-# directory of amplitude files
-os.chdir(home + '/Google Drive/music_project/data_processed/wav data (0.15 s)')
+# import sound amplitude data
+data = pd.read_csv('amplitude.csv')
 
-# a list of file names
 file_s1 = []; file_s2 = []
 for p in range(30):
-    file_s1.append('Trial' + str(p+1).zfill(2) + 'p1s1.csv')
-    file_s2.append('Trial' + str(p+1).zfill(2) + 'p1s2.csv')
+    s1 = data.loc[(data['session']==1) & (data['pair']==p) & (data['player']==1)]['amplitude']
+    s2 = data.loc[(data['session']==2) & (data['pair']==p) & (data['player']==1)]['amplitude']
+    file_s1.append(np.array(s1))
+    file_s2.append(np.array(s2))
 for q in range(30):
-    file_s1.append('Trial' + str(q+1).zfill(2) + 'p2s1.csv')
-    file_s2.append('Trial' + str(q+1).zfill(2) + 'p2s2.csv')
+    s1 = data.loc[(data['session']==1) & (data['pair']==q) & (data['player']==2)]['amplitude']
+    s2 = data.loc[(data['session']==2) & (data['pair']==q) & (data['player']==2)]['amplitude']
+    file_s1.append(np.array(s1))
+    file_s2.append(np.array(s2))
 
 # an empty array to store data
-## session, values (SRR, v_max, v_bar, v_prop, d_max, d_bar, d_prop, H),
+## session, values (SRR, d_max, d_bar, d_prop, v_max, v_bar, v_prop, H),
 ## player i, player j
 rec_together = np.empty((2, 8, 60, 60))
 
@@ -34,19 +35,15 @@ start = time.time()
 for i in range(60):
     for j in range(60):
         #session 1
-        data1 = pd.read_csv(file_s1[i])
-        data2 = pd.read_csv(file_s1[j])
-        foo1 = data1['amplitude']
-        foo2 = data2['amplitude']
+        foo1 = file_s1[i]
+        foo2 = file_s2[j]
         foo = foo1 + foo2
         symbol_foo = symbolize(foo, m)
         rec_together[0,:,i,j] = recurrence(symbol_foo, m)
 
         #session 2
-        data1 = pd.read_csv(file_s2[i])
-        data2 = pd.read_csv(file_s2[j])
-        foo1 = data1['amplitude']
-        foo2 = data2['amplitude']
+        foo1 = file_s2[i]
+        foo2 = file_s2[j]
         foo = foo1 + foo2
         symbol_foo = symbolize(foo, m)
         rec_together[1,:,i,j] = recurrence(symbol_foo, m)
@@ -57,5 +54,4 @@ for i in range(60):
             print(60*i+j+1, 'out of', 3600, 'done.', elapsed, 'min elasped.')
 
 # save
-os.chdir(home + '/Google Drive/music_project/data_processed')
-np.save('rec_together_m3.npy', rec_together)
+np.save('rec_together.npy', rec_together)
