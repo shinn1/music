f8372f881ca0a28ac12c15f364ecfb51a4ef01eb
shinnakayama <shinn407@gmail.com>
2019-11-10 12:19:20 -0800

    Update recurrence_pair.py

--- recurrence_pair.py
+++ recurrence_pair.py
@@ -5,24 +5,22 @@ import time
 from pathlib import Path
 
 # import functions for recurrence analysis
-home = str(Path.home())
-os.chdir(home + '/Google Drive/music_project/codes_python')
 from recurrence_functions import symbolize, TErec, MIrec
 
-# directory of amplitude files
-os.chdir(home + '/Google Drive/music_project/data_processed/wav data (0.15 s)')
+# import sound amplitude data
+data = pd.read_csv('amplitude.csv')
 
-# list of file names
-## first 30 is player 1, second 30 is player 2
-file_s1 = []
-file_s2 = []
+file_s1 = []; file_s2 = []
 for p in range(30):
-    file_s1.append('Trial' + str(p+1).zfill(2) + 'p1s1.csv')
-    file_s2.append('Trial' + str(p+1).zfill(2) + 'p1s2.csv')
+    s1 = data.loc[(data['session']==1) & (data['pair']==p) & (data['player']==1)]['amplitude']
+    s2 = data.loc[(data['session']==2) & (data['pair']==p) & (data['player']==1)]['amplitude']
+    file_s1.append(np.array(s1))
+    file_s2.append(np.array(s2))
 for q in range(30):
-    file_s1.append('Trial' + str(q+1).zfill(2) + 'p2s1.csv')
-    file_s2.append('Trial' + str(q+1).zfill(2) + 'p2s2.csv')
-
+    s1 = data.loc[(data['session']==1) & (data['pair']==q) & (data['player']==2)]['amplitude']
+    s2 = data.loc[(data['session']==2) & (data['pair']==q) & (data['player']==2)]['amplitude']
+    file_s1.append(np.array(s1))
+    file_s2.append(np.array(s2))
 # empty array to store data
 ## session, values (jointSRR, MI, TE), player i, player j
 rec_pair = np.empty((2, 3, 60, 60))
@@ -35,10 +33,8 @@ start = time.time()
 for i in range(60):
     for j in range(60):
         #session 1
-        data1 = pd.read_csv(file_s1[i])
-        data2 = pd.read_csv(file_s1[j])
-        foo1 = data1['amplitude']
-        foo2 = data2['amplitude']
+        foo1 = file_s1[i]
+        foo2 = file_s1[j]
         symbol_foo1 = symbolize(foo1, m)
         symbol_foo2 = symbolize(foo2, m)
         val1 = MIrec(symbol_foo1, symbol_foo2, m)     # jointSRR, recurrence MI
@@ -46,10 +42,8 @@ for i in range(60):
         rec_pair[0,:,i, j] = [val1[0], val1[1], val2]
 
         #session 2
-        data1 = pd.read_csv(file_s2[i])
-        data2 = pd.read_csv(file_s2[j])
-        foo1 = data1['amplitude']
-        foo2 = data2['amplitude']
+        foo1 = file_s2[i]
+        foo2 = file_s2[j]
         symbol_foo1 = symbolize(foo1, m)
         symbol_foo2 = symbolize(foo2, m)
         val1 = MIrec(symbol_foo1, symbol_foo2, m)
@@ -61,6 +55,3 @@ for i in range(60):
             elapsed = int((time.time() - start)/60)
             print(60*i+j+1, 'out of', len(file_s1) * len(file_s2), 'done.', elapsed, 'min elasped.')
 
-# save
-os.chdir(home + '/Google Drive/music_project/data_processed')
-np.save('rec_pair_m3.npy', rec_pair)
